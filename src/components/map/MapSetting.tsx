46b974b07c2a5b9035acaf02d79748bbd977e6eb
JuHyeong <wkdwngud963@gmail.com>
2025-09-02 21:03:44 +0900

    refactor: component에 있는 MapSetting 코드 LocationSetting 페이지로 이동

--- src/components/map/MapSetting.tsx
+++ src/components/map/MapSetting.tsx
@@ -64,29 +64,7 @@ export default function MapSetting() {
       width: '100%',
       height: '100%',
     }}>
-      <div style={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        zIndex: 10,
-        pointerEvents: 'none',
-      }}>
-        <MapCenterMarker />
-      </div>
-      <div style={{
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        width: '100%',
-        zIndex: 20,
-        pointerEvents: 'auto',
-      }}>
-        <ButtonBar />
-      </div>
       <div id='map' style={{ width: "100%", height: "100%" }} />
-      ;
-
     </div>
   )
 }
--- src/pages/LocationSetting.tsx
+++ src/pages/LocationSetting.tsx
@@ -1,9 +1,35 @@
 import MapSetting from '@/components/map/MapSetting.tsx'
+import { MapCenterMarker } from '@/components/map/MapCenterMarker.tsx'
+import ButtonBar from '@/components/map/ButtonBar.tsx'
 
 export default function LocationSetting() {
   return (
-    <>
+    <div style={{
+      position: 'relative',
+      width: '100%',
+      height: '100%',
+    }}>
+      <div style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: 10,
+        pointerEvents: 'none',
+      }}>
+        <MapCenterMarker />
+      </div>
+      <div style={{
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        zIndex: 20,
+        pointerEvents: 'auto',
+      }}>
+        <ButtonBar />
+      </div>
       <MapSetting />
-    </>
+    </div>
   )
 }

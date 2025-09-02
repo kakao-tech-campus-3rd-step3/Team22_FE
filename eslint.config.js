44c00301b2817ed30e05671785b7bfde17057869
JuHyeong <wkdwngud963@gmail.com>
2025-09-02 14:13:12 +0900

    chore: eslint 수정

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import prettier from 'eslint-plugin-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
@@ -15,34 +15,21 @@ export default tseslint.config([
       tseslint.configs.recommended,
     ],
     plugins: {
-      prettier,
-      'react-hooks': reactHooks,   // ✅ 플러그인 등록
-      'react-refresh': reactRefresh, // ✅ 플러그인 등록
+      prettier: prettierPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs['recommended-latest'].rules,
       ...reactRefresh.configs.vite.rules,
 
-      // ✅ Prettier와 연동
-      'prettier/prettier': 'warn',
-
-      // ✅ Google 스타일 기반 (Prettier와 충돌 없는 규칙만 유지)
-      quotes: ['error', 'single', { allowTemplateLiterals: true }],
-      semi: ['error', 'never'],
-      'comma-dangle': ['error', 'always-multiline'],
-      indent: ['error', 2, { SwitchCase: 1 }],
-      'max-len': [
-        'warn',
-        {
-          code: 100,
-          tabWidth: 2,
-          ignoreUrls: true,
-          ignoreStrings: true,
-          ignoreTemplateLiterals: true,
-        },
-      ],
-      'object-curly-spacing': ['error', 'always'],
-      'array-bracket-spacing': ['error', 'never'],
+      semi: 'off',
+      quotes: 'off',
+      'comma-dangle': 'off',
+      'max-len': 'off',
+      indent: 'off',
+      'object-curly-spacing': 'off',
+      'array-bracket-spacing': 'off',
     },
     languageOptions: {
       ecmaVersion: 2020,

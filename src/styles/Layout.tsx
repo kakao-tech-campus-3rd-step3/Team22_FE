4df551c2d3a9a63f9c58a190c426e460efe1ba08
JuHyeong <wkdwngud963@gmail.com>
2025-09-02 16:26:55 +0900

    style: 스크롤 비활성화

--- src/styles/Layout.tsx
+++ src/styles/Layout.tsx
@@ -5,7 +5,7 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="w-screen h-screen bg-gray-100 flex justify-center items-center">
-      <div className="w-[375px] h-[100%] bg-white shadow-lg rounded-xl overflow-auto">
+      <div className="w-[375px] h-[100%] bg-white shadow-lg rounded-xl overflow-hidden">
         {children}
       </div>
     </div>

e953afc9c6407ed2faa35263161b6d39388b3c10
JuHyeong <wkdwngud963@gmail.com>
2025-09-05 09:20:52 +0900

    style: marker 위치 중앙으로 변경

--- src/pages/LocationSetting.tsx
+++ src/pages/LocationSetting.tsx
@@ -47,7 +47,7 @@ export default function LocationSetting() {
         position: 'absolute',
         top: '50%',
         left: '50%',
-        transform: 'translate(-9%, -15%)',
+        transform: 'translate(-50%, -50%)',
         zIndex: 10,
         pointerEvents: 'none',
       }}>

525357371a1940174f02398e7ffc5e5b090deed2
JuHyeong <wkdwngud963@gmail.com>
2025-09-06 16:27:42 +0900

    feat: 시작 위치 설정 확인페이지 완성

---
+++ src/hooks/useKakaoStaticMap.ts
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from 'react'
+import startMarker from '@/assets/icons/startMarker.png';
+
+export default function useKakaoStaticMap({ latitude, longitude, loaded }) {
+  const mapContainerRef  = useRef(null);
+
+  useEffect(() => {
+    if (!loaded || !latitude || !longitude) return;
+
+    const startPosition = new window.kakao.maps.LatLng(latitude, longitude);
+
+    const mapOption = {
+      center: startPosition,
+      level: 3,
+    };
+
+    const map = new window.kakao.maps.Map(mapContainerRef.current, mapOption);
+
+    const markerPosition = startPosition;
+
+    const imageSrc = startMarker;
+    const imageSize = new window.kakao.maps.Size(50, 40);
+    const imageOption = { offset: new window.kakao.maps.Point(20, 40) };
+
+    const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+    const marker = new window.kakao.maps.Marker({
+      position: markerPosition,
+      image: markerImage,
+    });
+
+    marker.setMap(map);
+
+  }, [loaded, latitude, longitude]);
+
+  return {
+    mapContainerRef
+  };
+}

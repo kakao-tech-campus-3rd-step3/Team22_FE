7e7be7e462f9df887cf67aecb42062554ff162ac
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-09-07 21:09:53 +0900

    refacotr: 지도 상태 ref로 관리 및 타입 추가

--- src/hooks/useKakaoStaticMap.ts
+++ src/hooks/useKakaoStaticMap.ts
@@ -10,35 +10,45 @@ interface useKakaoStaticMapProps {
 
 export default function useKakaoStaticMap({ latitude, longitude, loaded }: useKakaoStaticMapProps) {
   const mapContainerRef  = useRef<HTMLDivElement | null>(null);
+  const mapInstanceRef = useRef<KakaoMap | null>(null);
+  const markerInstanceRef = useRef<KakaoMarker | null>(null);
 
   useEffect(() => {
-    if (!loaded || latitude == null || longitude == null) return;
+    if (!loaded || latitude == null || longitude == null || !mapContainerRef.current) return;
+    if (mapInstanceRef.current) return;
 
     const startPosition = new window.kakao.maps.LatLng(latitude, longitude);
-
     const mapOption = {
       center: startPosition,
       level: 3,
     };
 
     const map = new window.kakao.maps.Map(mapContainerRef.current, mapOption);
-
-    const markerPosition = startPosition;
+    mapInstanceRef.current = map;
 
     const imageSrc = startMarker;
     const imageSize = new window.kakao.maps.Size(MARKER_IMAGE_WIDTH, MARKER_IMAGE_HEIGHT);
     const imageOption = { offset: new window.kakao.maps.Point(MARKER_IMAGE_X, MARKER_IMAGE_Y) };
-
     const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 
     const marker = new window.kakao.maps.Marker({
-      position: markerPosition,
+      position: startPosition,
       image: markerImage,
     });
+    markerInstanceRef.current = marker;
 
     marker.setMap(map);
 
   }, [loaded, latitude, longitude]);
+
+  useEffect(() => {
+    if (!mapInstanceRef.current || !markerInstanceRef.current || latitude == null || longitude == null) return;
+
+    const newPosition = new window.kakao.maps.LatLng(latitude, longitude);
+
+    mapInstanceRef.current?.panTo(newPosition);
+    markerInstanceRef.current?.setPosition(newPosition);
+  }, [latitude, longitude]);
 
   return {
     mapContainerRef

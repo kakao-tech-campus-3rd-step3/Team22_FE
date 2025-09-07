aeb0fd031db0ca1570237730c752f4205622c079
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-09-08 00:06:11 +0900

    refactor: loading 상태 삭제 및 fetch 대신 axios 사용

--- src/hooks/useOpenWeather.ts
+++ src/hooks/useOpenWeather.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 interface useOpenWeatherProps {
   location: {
@@ -9,32 +10,25 @@ interface useOpenWeatherProps {
 
 export default function useOpenWeather({ location }: useOpenWeatherProps) {
   const [weather, setWeather] = useState<WeatherData | null>(null);
-  const [loading , setLoading] = useState(false);
 
   useEffect(() => {
     if (!location) return;
 
     const fetchWeather = async () => {
-      setLoading(true);
-
-      const url =
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${location.latitude}&lon=${location.longitude}&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}&units=metric&lang=kr`
       try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
+        const url =
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${location.latitude}&lon=${location.longitude}&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}&units=metric&lang=kr`
+        const response = await axios.get(url);
+        const data = response.data;
         setWeather(data);
       } catch (e) {
         setWeather(null);
-        console.error("날씨 정보 fetching 실패:", e);
+        console.error("날씨 정보 fetching 실패: ", e);
       }
     }
 
     fetchWeather();
-    setLoading(false);
   }, [location]);
 
-  return { weather, loading };
+  return { weather };
 }

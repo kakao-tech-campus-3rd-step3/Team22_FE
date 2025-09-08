8d4e32df2e22f77d731d81213341fe65dd2e6805
meem3443 <jinsr0235@gmail.com>
2025-09-08 14:10:05 +0900

    chore : zod form added

---
+++ src/types/petProfile.ts
@@ -0,0 +1,53 @@
+import { z } from 'zod'
+
+export const petProfileSchema = z.object({
+  selectedBreed: z.string().min(1, { message: '견종을 선택해주세요.' }),
+
+  gender: z.string().refine((value) => ['male', 'female'].includes(value), {
+    message: '유효한 성별이 아닙니다.',
+  }),
+
+  neutralize: z.string({}).refine((value) => ['yes', 'no'].includes(value), {
+    message: "중성화 여부는 'yes' 또는 'no'여야 합니다.",
+  }),
+
+  vaccinated: z.string({}).refine((value) => ['yes', 'no'].includes(value), {
+    message: "예방접종 여부는 'yes' 또는 'no'여야 합니다.",
+  }),
+  birthdate: z
+    .string()
+    .regex(/^\d{4}-\d{2}-\d{2}$/, '날짜 형식이 YYYY-MM-DD가 아닙니다.')
+    .refine((date) => {
+      const time = new Date(date).getTime()
+      if (isNaN(time)) return false
+      return true
+    }, '유효하지 않은 날짜입니다.')
+    .refine((date) => {
+      const inputDate = new Date(date)
+      const now = new Date()
+      return inputDate <= now
+    }, '생년월일이 현재보다 미래일 수 없습니다.'),
+
+  personality: z.string({}).refine((value) => ['extroverted', 'introverted'].includes(value), {
+    message: '유효한 성격 유형이 아닙니다.',
+  }),
+
+  dayWeather: z.array(z.string()).nonempty({ message: '낮 날씨를 선택해주세요.' }),
+  nightWeather: z.array(z.string()).nonempty({ message: '밤 날씨를 선택해주세요.' }),
+  preferredPaths: z.array(z.string()).nonempty({ message: '산책로를 선택해주세요.' }),
+
+  selectedDiseases: z.array(z.string()).optional(),
+
+  weight: z.preprocess(
+    (val) => {
+      if (typeof val === 'string') {
+        const parsed = Number(val)
+        return isNaN(parsed) ? val : parsed
+      }
+      return val
+    },
+    z.number().min(1, { message: '몸무게는 1kg 이상이어야 합니다.' }),
+  ),
+})
+
+export type PetProfile = z.infer<typeof petProfileSchema>

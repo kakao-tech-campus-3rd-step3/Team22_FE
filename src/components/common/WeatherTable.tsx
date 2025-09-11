9bda5068d2da57b4962ebbc290b2a0d3cc497960
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-09-11 19:59:35 +0900

    refactor : error, loading 줄 바꿈

--- src/components/common/WeatherTable.tsx
+++ src/components/common/WeatherTable.tsx
@@ -15,9 +15,9 @@ export default function WeatherTable() {
     );
   }
 
-  if (!weather || !weather.list) return <div>데이터가 없습니다.</div>;
   if (error) return <div>{error}</div>
   if (loading) return <div>날씨 정보를 불러오는 중...</div>;
+  if (!weather || !weather.list) return <div>데이터가 없습니다.</div>;
 
   return (
     <div className="w-full flex justify-center">

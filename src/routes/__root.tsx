d2519a436ab637909294dd44ad28433c5b97c1b6
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-09-11 15:24:46 +0900

    refactor : / 대신 |로 수정

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,7 +5,7 @@ export const Route = createRootRoute({
     <>
       <nav>
         <a href="/add-new-pet">펫정보테스트</a> | <a href="/location-setting">지도테스트</a> |
-        <a href="/walk-time-setting">시간 설정</a> / <a href="/map-setup">설정 완료</a>
+        <a href="/walk-time-setting">시간 설정</a> | <a href="/map-setup">설정 완료</a>
       </nav>
       <hr />
       <Outlet />

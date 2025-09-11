9c6b8b483f4298f0bf5f183b69e01411859a98eb
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-09-11 14:28:30 +0900

    refactor : merge branch 'refactor/week-5-feedback' into feat/MapSetupComplete

--- src/components/map/MapSetting.tsx
+++ src/components/map/MapSetting.tsx
@@ -1,11 +1,7 @@
 export default function MapSetting(props: { mapRef: React.RefObject<HTMLDivElement | null> }) {
   return (
     <div className="relative w-full h-full">
-<<<<<<< HEAD
       <div id="map" ref={props.mapRef} className="w-full h-full" />
-=======
-      <div id='map' ref={mapRef} style={{ width: "100%", height: "100%" }} />
->>>>>>> refactor/week-5-feedback
     </div>
   )
 }

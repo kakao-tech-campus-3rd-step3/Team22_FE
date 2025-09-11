1ac4d5ddbd1c96258e1d1a0e82933487d359bd43
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-09-11 15:19:58 +0900

    refactor : root에 MapSetupCompletePage route 추가

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,8 +4,8 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <nav>
-        <a href="/add-new-pet">펫정보테스트</a> | <a href="location-setting">지도테스트</a> |
-        <a href="/walk-time-setting">시간 설정</a>
+        <a href="/add-new-pet">펫정보테스트</a> | <a href="/location-setting">지도테스트</a> |
+        <a href="/walk-time-setting">시간 설정</a> / <a href="/map-setup">설정 완료</a>
       </nav>
       <hr />
       <Outlet />

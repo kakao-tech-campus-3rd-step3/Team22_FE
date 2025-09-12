63f0ea0db422906179386250c9a529c0aef193c8
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-09-12 16:45:32 +0900

    chore : move front tanstackRouter()

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  plugins: [react(), tanstackRouter({ target: 'react', autoCodeSplitting: true }), tailwindcss()],
+  plugins: [tanstackRouter({ target: 'react', autoCodeSplitting: true }), react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),

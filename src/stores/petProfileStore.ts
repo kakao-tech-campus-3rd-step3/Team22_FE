167ae31a2f6150143778cec1b01a2f65e255630b
meem3443 <jinsr0235@gmail.com>
2025-09-16 15:44:23 +0900

    : remove useless code

--- src/stores/petProfileStore.ts
+++ src/stores/petProfileStore.ts
@@ -35,17 +35,3 @@ export const usePetProfileStore = create<PetProfileStore>()(
     },
   ),
 )
-
-export const defaultPetProfile: PetProfile = {
-  selectedBreed: '...' as any, // BREED_VALUES 중 하나로 기본값 지정
-  gender: 'male',
-  neutralize: 'no',
-  vaccinated: 'no',
-  birthdate: '2020-01-01',
-  personality: 'extroverted',
-  dayWeather: ['맑음'], // 예시 값
-  nightWeather: ['맑음'],
-  preferredPaths: ['공원'],
-  selectedDiseases: [],
-  weight: 3,
-}

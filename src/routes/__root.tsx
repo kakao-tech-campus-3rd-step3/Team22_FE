2477f9c7859aa70fdb1c23a1796f226c2049f71e
김진서 <jinsr0235@gmail.com>
2025-09-19 18:56:25 +0900

    feat : add MyPage

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,11 +7,6 @@ export function RootLayout() {
 
   return (
     <>
-      <nav>
-        <a href="/add-new-pet">펫정보테스트</a> | <a href="/location-setting">지도테스트</a> |
-        <a href="/walk-time-setting">시간 설정</a> | <a href="/map-setup">설정 완료</a>
-      </nav>
-      <hr />
       <div className="flex justify-center items-center min-h-screen bg-neutral-800 font-sans">
         <div className="flex flex-col items-center w-[390px] h-[844px] bg-[#121212] text-white shadow-2xl rounded-3xl overflow-hidden">
           <div className="flex-1 w-full px-6 pt-6 overflow-y-auto">

a51d91128dda22bfe107d2fc56b2e2f9da28e9cd
김진서 <jinsr0235@gmail.com>
2025-09-22 16:53:48 +0900

    refactor : props.hsize

--- src/components/common/LoadingBox.tsx
+++ src/components/common/LoadingBox.tsx
@@ -2,10 +2,8 @@ import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 
 export default function LoadingBox(props: { hsize: string }) {
   return (
-    <div className="w-full flex justify-center">
-      <div
-        className={`w-full max-w-lg h-${props.hsize} overflow-auto rounded-lg no-scrollbar bg-zinc-800 flex flex-col items-center justify-center`}
-      >
+    <div className={`w-full flex justify-center h-${props.hsize}`}>
+      <div className="w-full max-w-lg  overflow-auto rounded-lg no-scrollbar bg-zinc-800 flex flex-col items-center justify-center">
         <AiOutlineLoading3Quarters className="animate-spin text-4xl text-white" />
       </div>
     </div>

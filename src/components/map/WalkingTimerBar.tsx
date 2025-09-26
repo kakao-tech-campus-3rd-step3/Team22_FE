6316b5a2c4a228e25913f080fd463bd14795fb0e
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-09-26 09:21:19 +0900

    refactor: delete ()

--- src/components/map/WalkingTimerBar.tsx
+++ src/components/map/WalkingTimerBar.tsx
@@ -35,9 +35,9 @@ export default function WalkingTimerBar(props: {
     props.setIsActive(false)
   }
 
-  const onHandleEndWalking = (() => {
+  const onHandleEndWalking = () => {
     props.handleTriggerEnd()
-  })
+  }
 
   return (
     <div className="w-full">

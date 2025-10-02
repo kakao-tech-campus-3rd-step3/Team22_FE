7e57699fa156d3f770e57e83be1bc189c57f043a
김진서 <jinsr0235@gmail.com>
2025-10-02 13:18:36 +0900

    feat : add name zod checking

--- src/pages/AddNewPetPageSections/ProfileSection.tsx
+++ src/pages/AddNewPetPageSections/ProfileSection.tsx
@@ -1,7 +1,7 @@
 import { FaDog } from 'react-icons/fa'
 import IconPlus from '@/components/common/IconPlus'
 
-function ProfileSection() {
+function ProfileSection(props: { name: string; setName: (value: string) => void }) {
   return (
     <div className="flex flex-col items-center gap-2">
       <div className="relative w-24 h-24 bg-neutral-700 rounded-full flex items-center justify-center">
@@ -12,6 +12,8 @@ function ProfileSection() {
       </div>
       <div className="flex items-center">
         <input
+          value={props.name}
+          onChange={(e) => props.setName(e.target.value)}
           type="text"
           placeholder="멍멍이"
           className="text-center p-2 rounded-md text-lg w-28 h-auto focus:bg-neutral-700 focus:outline-none bg-neutral-700"

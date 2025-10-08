ded40883e2c7b1bb523ef4996e0329431b5ac407
김진서 <jinsr0235@gmail.com>
2025-10-08 17:53:21 +0900

    chore : api replace to mock for api not ready

--- src/components/map/WalkingEndModalComponent.tsx
+++ src/components/map/WalkingEndModalComponent.tsx
@@ -2,6 +2,8 @@ import { formatTime, getTotalSeconds } from '@/utils/timeCalculation.ts'
 import { useNavigate } from '@tanstack/react-router'
 import { walkingResultSchema, type WalkingResultState } from '@/types/routeResult.ts'
 import useCreatePath from '@/hooks/useCreatePath.ts'
+import { walkingPath } from '@/mocks/testdata.ts'
+import { useSetupStore } from '@/stores/setupStore'
 
 export default function WalkingEndModalComponent(props: {
   totalDistance: number
@@ -10,9 +12,10 @@ export default function WalkingEndModalComponent(props: {
   setEndModal: (value: boolean) => void
   handleEndWalking: () => void
 }) {
-  const { totalDistance, elapsedTime, route, setEndModal, handleEndWalking } = props;
-  const navigate = useNavigate();
+  const { totalDistance, elapsedTime, route, setEndModal, handleEndWalking } = props
+  const navigate = useNavigate()
   const createPathMutation = useCreatePath()
+  const setRouteDrawDone = useSetupStore((state) => state.setRouteDrawDone)
 
   const handleSubmitResult = () => {
     const rawResult = {
@@ -20,20 +23,23 @@ export default function WalkingEndModalComponent(props: {
       walkingTime_sec: getTotalSeconds(elapsedTime),
       path: route,
     }
+    console.log('rawResult', rawResult)
+    const mockRoute = walkingPath
 
-    const parsed = walkingResultSchema.safeParse(rawResult)
+    const parsed = walkingResultSchema.safeParse(mockRoute)
 
     if (!parsed.success) {
-      alert("산책 기록 데이터가 유효하지 않습니다.")
+      alert('산책 기록 데이터가 유효하지 않습니다.')
       return
     }
 
     const result: WalkingResultState = parsed.data
 
     createPathMutation.mutate(result)
+    setRouteDrawDone(true)
 
     handleEndWalking()
-    navigate({ to: "/" })
+    navigate({ to: '/' })
   }
 
   return (

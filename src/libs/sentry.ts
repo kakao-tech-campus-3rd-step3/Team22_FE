cf095db615e77a733455d43fcafe4a822e4a2aa2
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-13 13:16:22 +0900

    refactor: sentry init

--- src/libs/sentry.ts
+++ src/libs/sentry.ts
@@ -1,9 +1,8 @@
 import * as Sentry from '@sentry/browser'
-import * as process from 'process'
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
-  environment: process.env.MODE,
+  environment: import.meta.env.MODE,
   tracesSampleRate: 1.0,
 })
 

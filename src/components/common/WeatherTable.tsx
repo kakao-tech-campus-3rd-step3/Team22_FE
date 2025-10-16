527f7dd4faf5f46b93f02018eba79f021013cbfa
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-17 02:00:26 +0900

    refactor: weather Icon

--- src/components/common/WeatherTable.tsx
+++ src/components/common/WeatherTable.tsx
@@ -7,7 +7,16 @@ import neutral from '@/assets/walkingIcons/neutral_line.svg'
 import bad from '@/assets/walkingIcons/bad_line.svg'
 import veryBad from '@/assets/walkingIcons/very_bad_line.svg'
 import { BAD, GOOD, NEUTRAL, VERY_GOOD } from '@/constants/walkingScore.ts'
-
+import clearNight from '@/assets/weatherIcons/clear_night.svg'
+import clearDay from '@/assets/weatherIcons/clear_day.svg'
+import cloudyDay from '@/assets/weatherIcons/cloudy_day.svg'
+import cloudyNight from '@/assets/weatherIcons/cloudy_night.svg'
+import rainyDay from '@/assets/weatherIcons/rainy_day.svg'
+import rainyNight from '@/assets/weatherIcons/rainy_night.svg'
+import snowyDay from '@/assets/weatherIcons/snowy_day.svg'
+import snowyNight from '@/assets/weatherIcons/snowy_night.svg'
+import windDay from '@/assets/weatherIcons/wind_day.svg'
+import windNight from '@/assets/weatherIcons/wind_night.svg'
 
 export default function WeatherTable() {
   const { initialLocation, status } = useLocation()
@@ -43,6 +52,34 @@ export default function WeatherTable() {
     }
   }
 
+  const handleWeatherIcons = (weather : string) => {
+    if (weather.includes("NIGHT")) {
+      if (weather.includes("CLEAR")) {
+        return <img src={clearNight} alt="clear Night" className="mx-auto" />
+      } else if (weather.includes("CLOUDY")) {
+        return <img src={cloudyNight} alt="cloudy Night" className="mx-auto" />
+      } else if (weather.includes("RAINY")) {
+        return <img src={rainyNight} alt="rainy Night" className="mx-auto" />
+      } else if (weather.includes("SNOWY")) {
+        return <img src={snowyNight} alt="snowy Night" className="mx-auto" />
+      } else if (weather.includes("WIND")) {
+        return <img src={windNight} alt="wind Night" className="mx-auto" />
+      }
+    } else {
+      if (weather.includes("CLEAR")) {
+        return <img src={clearDay} alt="clear Day" className="mx-auto" />
+      } else if (weather.includes("CLOUDY")) {
+        return <img src={cloudyDay} alt="cloudy Day" className="mx-auto" />
+      } else if (weather.includes("RAINY")) {
+        return <img src={rainyDay} alt="rainy Day" className="mx-auto" />
+      } else if (weather.includes("SNOWY")) {
+        return <img src={snowyDay} alt="snowy Day" className="mx-auto" />
+      } else if (weather.includes("WIND")) {
+        return <img src={windDay} alt="wind Day" className="mx-auto" />
+      }
+    }
+  }
+
   return (
     <div className="w-full flex justify-center">
       <div className="w-full max-w-lg h-72 overflow-auto rounded-lg no-scrollbar border border-zinc-700">
@@ -66,7 +103,7 @@ export default function WeatherTable() {
             return (
               <tr key={item.weatherDetail.time} className="text-center text-xs font-bold border-t border-zinc-700">
                 <td className="py-2">{`${day}일 ${hour}시`}</td>
-                <td className="py-2">{item.weatherDetail.condition}</td>
+                <td className="py-2">{handleWeatherIcons(item.weatherDetail.condition)}</td>
                 <td className="py-2">{item.weatherDetail.temperature.toFixed(1)}</td>
                 <td className="py-2">{(item.weatherDetail.precipitationProbability * 100).toFixed(0)}</td>
                 <td className="py-2">{handleWalkingIcons(item.walkScore)}</td>

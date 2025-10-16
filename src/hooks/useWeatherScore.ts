c21a10b138d6cbabf2b5b920fefac6dabc108f3e
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-17 00:04:41 +0900

    refactor: get initial location

--- src/hooks/useWeatherScore.ts
+++ src/hooks/useWeatherScore.ts
@@ -15,14 +15,16 @@ type HourlyForecast = {
 };
 
 export default function useWeatherScore(props: { initialLocation: { latitude: number; longitude: number } | null }) {
+  const { initialLocation } = props;
   const { latitude, longitude } = props.initialLocation ?? {};
 
   return useQuery<HourlyForecast[]> ({
-    queryKey: ['weatherScore', latitude, longitude, status],
+    queryKey: ['weatherScore', latitude, longitude],
     queryFn: async () => {
       const response = await getWeather(latitude!, longitude!)
       return response.data.hourlyForecasts
     },
     staleTime: 1000 * 6 * 5,
+    enabled: !!initialLocation,
   })
 }
